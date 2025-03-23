c72b9f60baa11e34feb00b8430b6645eea2a18ab
Abdul-Rafay-AR <142048280+Abdul-Rafay-AR@users.noreply.github.com>
2025-03-23 08:15:23 +0500

    remove fallacies with longer names

--- javascript/fallacies.js
+++ javascript/fallacies.js
@@ -1,8 +1,9 @@
 var relevance = [
-    "circumstantial ad hominem",
-    "abusive ad hominem",
+    // "circumstantial ad hominem",
+    // "abusive ad hominem",
+    "ad hominem",
     "tu quoque",
-    "two wrongs make a right",
+    // "two wrongs make a right",
     "straw man",
     "appeal to force",
     "appeal to people",
@@ -13,12 +14,12 @@ var relevance = [
 ];
 
 var relevance_fallacies = [
-    `Tom: This Toyota Prius will give you the best mileage out of any car in this lot!\n
-    Jerry: I don’t believe you. You’re just saying that because you get the highest commission on selling Toyotas.`,
+    // `Tom: This Toyota Prius will give you the best mileage out of any car in this lot!\n
+    // Jerry: I don’t believe you. You’re just saying that because you get the highest commission on selling Toyotas.`,
     `Malik: Personally, I think your suit would look better buttoned up, it would complement your shirt’s color better that way.\n
     Ahmed: I’m not taking fashion advice from you while you’re sitting in those ugly pajamas.`,
     `DeNiro says that gambling is the riskiest way to get rich. He claims the odds at casinos are always stacked against you. This is meaningless coming from the guy who made $7 million dollars at the slot machine.`,
-    `Babar did nothing wrong by bribing the NADRA officer. If he hadn’t, he would still be waiting for an appointment. It’s the only way to get things done on time.`,
+    // `Babar did nothing wrong by bribing the NADRA officer. If he hadn’t, he would still be waiting for an appointment. It’s the only way to get things done on time.`,
     `Chang: The technology billionaires such as Elon Musk, Jeff Bezos and Mark Zuckerberg are destroying our planet with their incessant need for growth, even when it comes at the expense of the ecology.\n
     Matt: So you have a problem with rich people. You want all of us to start paying 50% of our income in taxes to the government. I ain’t a communist, buddy. `,
     `Arthur: What’re we doin’ Dutch? We’ve been on the run from the law for over a year now.\n
@@ -36,26 +37,26 @@ var relevance_fallacies = [
 var insufficient_evidence = [
     "appeal to ignorance",
     "false dilemma",
-    "inappropriate appeal to authority",
+    // "inappropriate appeal to authority",
     "false cause",
     "slippery slope",
     "loaded question",
     "hasty generalization",
-    "fallacy of inconsistency",
+    // "fallacy of inconsistency",
     "weak analogy",
 ];
 
 var insufficient_evidence_fallacies = [
     `We have never seen a horse nor a monkey surviving in outer space. That is conclusive proof that both horses and monkeys cannot survive in outer space.`,
     `If I jump off the third floor right now, I am either going to die on impact or live to tell the tale of my stupidity.`,
-    `The Mi Band is the most fashionable watch you can buy in the market right now because everyone on Pakistan’s hockey team wears one. `,
+    // `The Mi Band is the most fashionable watch you can buy in the market right now because everyone on Pakistan’s hockey team wears one. `,
     `We didn’t have any of this global warming stuff back when pirates were ruling the seas. Is it not clear? We must bring back pirates to control global warming.`,
     `Schools should not implement student uniforms, because it will introduce a trend of controlling policies. Next thing we know, schools get to decide what students should eat, when they should wake up and what they’re allowed to watch.`,
     `"The trunk of a Hyundai Tucson is huge. I could fit in there."
     "What are you on about? It only has space for a toddler."
     "So you haven’t stopped stuffing toddlers in there?"`,
     `I just interviewed my first ever candidate from LUMS, and my God was he arrogant. I don’t think I will be accepting any applications coming from this community of snobs.`,
-    "fallacy of inconsistency - couldn't find an",
+    // "fallacy of inconsistency - couldn't find an",
     `Amir Khan, one of the most successful Bollywood actors, was born into a Muslim family and so was I. Amir Khan is 5’6”, and so am I. We even share the same last name. This practically guarantees my success in Bollywood.`,
 ];
 
